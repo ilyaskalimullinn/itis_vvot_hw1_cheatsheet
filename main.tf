0bf2a6a02ee8b41d3e882f43dbea17ec539f0b94
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2025-01-04 15:57:50 +0300

    add basic handler and cloud function code uploading

--- main.tf
+++ main.tf
@@ -8,5 +8,38 @@ terraform {
 }
 
 provider "yandex" {
-  zone = "ru-central1-d"
+  cloud_id                 = "b1g71e95h51okii30p25"
+  folder_id                = "b1g9896fnt23hf2ohrrp"
+  service_account_key_file = pathexpand("~/.yc-keys/key.json")
+  zone                     = "ru-central1-d"
+}
+
+resource "yandex_function_iam_binding" "cloud_func_publicity" {
+  function_id = yandex_function.cloud_func.id
+  role        = "serverless.functions.invoker"
+
+  members = [
+    "system:allUsers",
+  ]
+}
+
+resource "yandex_function" "cloud_func" {
+  name       = "function-homework-1"
+  user_hash  = archive_file.code_zip.output_sha256
+  runtime    = "python312"
+  entrypoint = "index.handler"
+  memory     = 128
+  content {
+    zip_filename = archive_file.code_zip.output_path
+  }
+}
+
+resource "archive_file" "code_zip" {
+  type        = "zip"
+  output_path = "func.zip"
+  source_dir  = "src"
+}
+
+output "function_url" {
+  value = "https://functions.yandexcloud.net/${yandex_function.cloud_func.id}"
 }
